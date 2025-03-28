abe31e3b481099a7d5936e892ad842d945f3bb87
Terrenoss <61914688+Terrenoss@users.noreply.github.com>
2025-03-28 08:49:21 +0100

    Test PlayerHeal Animation

--- Assets/Scripts/Enemy_Combat.cs
+++ Assets/Scripts/Enemy_Combat.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Enemy_Combat : MonoBehaviour
 {
     
-    public int damage = 1;
+    public int damage = 30;
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     
-    public int currentHealth;
-    public int maxHealth;
+    public float currentHealth;
+    public float maxHealth;
+    
+    private float lerpSpeed = 0.5f;
+    public Image healthSlider;
+    public Image easeHealthSlider;
+
+    
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        // if (healthSlider.fillAmount != currentHealth/maxHealth)
+        // {
+        //     healthSlider.fillAmount = currentHealth/maxHealth;
+        // }
+        //
+        // if(healthSlider.fillAmount != easeHealthSlider.fillAmount)
+        // {
+        //     easeHealthSlider.fillAmount = Mathf.Lerp(easeHealthSlider.fillAmount, currentHealth/maxHealth, lerpSpeed);
+        // }
+    }
 
     public void ChangeHealth(int amount)
     {
@@ -17,6 +42,22 @@ public class PlayerHealth : MonoBehaviour
         if(currentHealth <= 0)
         {
             gameObject.SetActive(false);
+        }
+        
+        UpdatePlayerHB();
+    }
+    
+    
+    public void UpdatePlayerHB()
+    {
+        if(healthSlider.fillAmount != currentHealth/maxHealth)
+        {
+            healthSlider.fillAmount = currentHealth/maxHealth;
+        }
+        
+        if(healthSlider.fillAmount != easeHealthSlider.fillAmount)
+        {
+            easeHealthSlider.fillAmount = Mathf.Lerp(easeHealthSlider.fillAmount, healthSlider.fillAmount, lerpSpeed);
         }
     }
 }
